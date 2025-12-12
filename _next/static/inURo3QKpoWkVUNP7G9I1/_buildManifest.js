ad945c6997ee7a2afbcaabeb1bac897fec2a8dea
osvaldoski <osvaldo143143@gmail.com>
2025-12-12 16:10:36 -0300

    Updates

--- _next/static/inURo3QKpoWkVUNP7G9I1/_buildManifest.js
+++ _next/static/inURo3QKpoWkVUNP7G9I1/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/gruas68//_next/:path+",
+        "destination": "/gruas68/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
